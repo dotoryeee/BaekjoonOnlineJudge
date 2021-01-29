1e6ccb22df26a48e8d46c0a3b8d49bf0b7924368
dotoryeee <bayer@kakao.com>
2021-01-29 15:14:52 +0900

    Update 1157.py

--- 1157.py
+++ 1157.py
@@ -1,21 +1,26 @@
 import sys
 
-receive = list(sys.stdin.readline().rstrip())
+def start():
+    word = []#소문자 데이터
+    for i in receive:
+        word.append(i.lower())
+    receive.clear()
 
-word = []#소문자 데이터
-for i in receive:
-    word.append(i.lower())
-receive.clear()
+    count = {}#알파벳 카운팅
+    for i in word:
+        if i not in count:
+            count[i] = 1
+        else:
+            count[i] += 1
 
-count = {}#알파벳 카운팅
-for i in word:
-    if i not in count:
-        count[i] = 1
+    final_data = sorted(count, key=lambda x : count[x], reverse=True)
+    if count[final_data[0]] == count[final_data[1]]:
+        print('?')
     else:
-        count[i] += 1
+        print(final_data[0].upper())
 
-final_data = sorted(count, key=lambda x : count[x], reverse=True)
-if count[final_data[0]] == count[final_data[1]]:
-    print('?')
+receive = list(sys.stdin.readline().rstrip())
+if len(receive) == 1:
+    print(receive[0].upper())
 else:
-    print(final_data[0].upper())
+    start()
